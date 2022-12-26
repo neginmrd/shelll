ec6f62ecf743b8c44aeefa03deb83dea9974d736
Salma <salma.eztt@gmail.com>
2022-12-26 14:22:48 +0330

    proccess own commands added

--- shell.c
+++ shell.c
@@ -5,6 +5,8 @@
 
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define MAX_LINES_IN_FILE 500
+#define MAX_CHARACTERS_IN_LINE 100
 
 char *inputString(FILE* fp, size_t size) {
 //The size is extended by the input with the value of the provisional
@@ -32,12 +34,49 @@ void printCurrentDir() {
     printf(">>> " ANSI_COLOR_RESET);
 }
 
+int isOwnCommand(char input[]){
+    char ownCommands[6][15] = {"headlines", "freq", "noSpace", "noComment", "howmanylines", "head10"};
+    int i = 0;
+    while(i < 6){
+        if(!strcmp(ownCommands[i], input)){
+            printf("found it in own commands.\n");
+            return(1);
+        }
+        i++;
+    }
+    return(0);
+}
+
+int processOwnCommands(char input[]) {
+    char *command = strtok(input, " ");
+    char *filename = strtok(NULL, " ");
+    if (!isOwnCommand(command)) return(0);
+
+    // read lines from file and put them in array
+    FILE *fptr = fopen(filename, "r");
+    if (fptr == NULL){
+        printf("Could not open file.\n");
+        return(1);
+    }
+    char buffer[MAX_LINES_IN_FILE][MAX_CHARACTERS_IN_LINE];
+    int count = 0; // number of lines
+    char tmp[MAX_CHARACTERS_IN_LINE];
+    while(fgets(tmp, MAX_CHARACTERS_IN_LINE, fptr) != NULL){
+        tmp[strcspn(tmp, "\n")] = 0;
+        strcpy(buffer[count], tmp);
+        count++;
+    }
+    fclose(fptr);
+    return(1);
+}
+
 int main() {
     int running = 1;
     char *line;
     while(running) {
         printCurrentDir();
         line = inputString(stdin, 10);
+        processOwnCommands(line);
         free(line);
     }
     return(0);
